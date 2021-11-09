fe8081609fb32cece2d676d4c2c757a2b2912c35
zsomle99 <78908383+zsomle99@users.noreply.github.com>
2021-11-09 15:06:44 +0100

    Update xPathHC1Y8Y.java

--- .hc1y8y1109/xPathHC1Y8Y/src/xpathhc1y8y1109/xPathHC1Y8Y.java
+++ .hc1y8y1109/xPathHC1Y8Y/src/xpathhc1y8y1109/xPathHC1Y8Y.java
@@ -22,13 +22,11 @@ public class xPathHC1Y8Y {
 		factory.setNamespaceAware(true);
 		DocumentBuilder builder = factory.newDocumentBuilder();
 		Document doc = builder.parse("studentHC1Y8Y.xml");
-
 		doc.getDocumentElement().normalize();
 
 		XPathFactory xpathfactory = XPathFactory.newInstance();
 		XPath xpath = xpathfactory.newXPath();
-
-		System.out.println("Print out the firstnames from the ids");
+		System.out.println("Print out the firstnames of the students");
 
 		XPathExpression expr = xpath.compile("//student[@id>0]/firstname/text()");
 		Object result = expr.evaluate(doc, XPathConstants.NODESET);
@@ -38,31 +36,34 @@ public class xPathHC1Y8Y {
 		}
 		System.out.println("End of firstname");
 		System.out.println();
+		
+		System.out.println("Print out the lastnames of the students");
 		XPathExpression expr2 = xpath.compile("//student[@id>0]/lastname/text()");
 		Object result2 = expr2.evaluate(doc, XPathConstants.NODESET);
 		NodeList nodes2 = (NodeList) result2;
 		for (int j = 0; j < nodes2.getLength(); j++) {
 			System.out.println(nodes2.item(j).getNodeValue());
 		}
-		
 		System.out.println("End of lastname");
 		System.out.println();
+		
+		System.out.println("Print out the nicknames of the students");
 		XPathExpression expr3 = xpath.compile("//student[@id>0]/nickname/text()");
 		Object result3 = expr3.evaluate(doc, XPathConstants.NODESET);
 		NodeList nodes3 = (NodeList) result3;
 		for (int k = 0; k < nodes3.getLength(); k++) {
 			System.out.println(nodes3.item(k).getNodeValue());
 		}
-		
 		System.out.println("End of nickname");
 		System.out.println();
+		
+		System.out.println("Print out the ages of the students");
 		XPathExpression expr4 = xpath.compile("//student[@id>0]/age/text()");
 		Object result4 = expr4.evaluate(doc, XPathConstants.NODESET);
 		NodeList nodes4 = (NodeList) result4;
 		for (int l = 0; l < nodes4.getLength(); l++) {
 			System.out.println(nodes4.item(l).getNodeValue());
 		}
-		
 		System.out.println("End of age");
 	}
 }
